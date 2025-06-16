1bf7e47279ee6a5c4af91464b599861f8fecbc73
1912063 <g2212044@tcu.ac.jp>
2025-06-16 18:25:33 +0900

    Complete plot_from_saved_result.m, LOGGER.m
    ・もうちょっとできたら嬉しいけど，ひとまず説明も書いて皆が使いやすいようにした

--- plot/plot_from_saved_result.m
+++ plot/plot_from_saved_result.m
@@ -1,25 +1,38 @@
-%% Explanation
-% 2025/06 Created by KOSEKI
+%% 説明
+% 2025/06 作成者：小関
+% Exp / Simデータをプロットすることができるファイル
+% 最初は全てのセクションを実行する．
+% データの読み込みができたら，プロットセクションだけ実行すれば手間が省ける．
+% プロットセクションのsettingsを変更して調整することでmainGUIの画面上とは違ういい感じのグラフが取れる
+
+% 分からないことや追加したい機能などあったらSlackで聞いてください
+% 凡例に関しては，書き方・位置を要検討
+% プロットしたいフェーズを選べるようになると嬉しい
 
 %% 初期化&パスの設定
-clc; clear;
-tmp = matlab.desktop.editor.getActive;
-cd(strcat(fileparts(tmp.Filename), '../../../'));
+clear all
+cf = pwd;
+
+if contains(mfilename('fullpath'), "mainGUI")
+    cd(fileparts(mfilename('fullpath')));
+else
+    tmp = matlab.desktop.editor.getActive;
+    cd(fileparts(erase(tmp.Filename, "plot\plot_from_saved_result.m")));
+end
+
 [~, tmp] = regexp(genpath('.'), '\.\\\.git.*?;', 'match', 'split');
 cellfun(@(xx) addpath(xx), tmp, 'UniformOutput', false);
-
-clear all
-clc
+close all hidden; clear; clc;
+userpath('clear');
 %% データの読み込み
 fprintf('MATファイルを選択してください:')
-if ~exist('filename', 'var')
-    [filename, pathname] = uigetfile('*.mat', 'MATファイルを選択してください');
-end
+[filename, pathname] = uigetfile('*.mat', 'MATファイルを選択してください');
 fprintf(filename);
 fullpath = fullfile(pathname, filename);
 logger = LOGGER(fullpath);
 
 %% プロット
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% settings %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fsave = 0;
 % [Recomendation] Initially, you should check the figure with fsave = 0, then chose save style.
@@ -29,20 +42,24 @@ fsave = 0;
 % 2:save as ".png"
 % 3:save as ".pdf"
 
-ftitle = 0; % defalt=1 -> with figure title
+ftitle = 0; % defalt=1 -> グラフタイトルあり
+settings.fcolor = 1; % default=1 -> フェーズごとの背景色あり
 
 %%%%%%%%%%%%%%%%%%%%%%%% chose target %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% settings.target = ["p", "q", "v", "w", "input", "p1-p2", "p1-p2-p3"];
-settings.target = ["p"];
-%                                                                   %
+settings.target = ["p", "q", "v", "w", "input", "p1-p2", "p1-p2-p3"];
+% プロットしたいグラフの情報                                          %
 % p: position    q: angle    v: velocity    w: angular velocity     %
 % input: controller input                                           %
 % p1-p2: x-y 2D plot    p1-p2-p3: x-y-z 3D plot                     %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-settings.fontsize = 18;      % default=11
+settings.fontsize = 18;    % default=11
 settings.linewidth = 2;    % default=0.5
 settings.agent_id = 1;
+
+% estimator, sensor, reference, (plant) どの値を表示するかは
+% 途中のキーボード入力で決定します．
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if logger.fExp == 1
@@ -52,39 +69,76 @@ else
 end
 
 for i=1:length(settings.target)
-    fcolor = 1;
-    if settings.target(i)=="q"
-        tmp = settings.attribute;
-        tmp(3) = [];
-    elseif settings.target(i)=="v"
-        tmp = settings.attribute;
-        tmp(2) = [];
-    elseif settings.target(i)=="w"
-        tmp = settings.attribute;
-        tmp(2:3) = [];
-    elseif settings.target(i)=="input"
-        tmp = settings.attribute;
-        tmp(2:3) = [];
-    elseif settings.target(i)=="p1-p2" || settings.target(i)=="p1-p2-p3"
-        tmp = settings.attribute;
-        fcolor = 0;
-    else
-        tmp = settings.attribute;
+    fcolor = settings.fcolor;
+    switch settings.target(i)
+        case "p"
+            ylabel = "Position [m]";
+            tmp = settings.attribute;
+        case "q"
+            ylabel = "Attitude [rad]";
+            tmp = settings.attribute;
+            tmp(3) = []; % "esr"の内，無いものを消去
+        case "v"
+            ylabel = "Velocity [m/s]";
+            tmp = settings.attribute;
+            tmp(2) = [];
+        case "w"
+            ylabel = "Angular velocity [rad/s]";
+            tmp = settings.attribute;
+            tmp(2:3) = [];
+        case "input"
+            ylabel = "Controller input [N]";
+            tmp = settings.attribute;
+            tmp(2:3) = [];
+        case "p1-p2"
+            xlabel = "x [m]";
+            ylabel = "y [m]";
+            tmp = settings.attribute;
+            fcolor = 0;
+        case "p1-p2-p3"
+            xlabel = "x [m]";
+            ylabel = "y [m]";
+            zlabel = "z [m]";
+            tmp = settings.attribute;
+            fcolor = 0;
     end
     att = select_attribute(settings.target(i), tmp);
     logger.plot({settings.agent_id, settings.target(i), att}, ...
-        fig_num=i, color=fcolor, ...
-        fontsize=settings.fontsize, linewidth=settings.linewidth)
-    if ftitle == 0
-        title([])
-    end
+        'fig_num',i, 'color',fcolor, ...
+        'FontSize',settings.fontsize, 'Linewidth',settings.linewidth)
 
+    fig = gcf;
+    ax = gca;
+
+    chars = string(split(att, ""));
+    chars(chars == "") = [];
+    switch settings.target(i)
+        case "p1-p2"
+            set(ax.XLabel, 'String', xlabel)
+            set(ax.YLabel, 'String', ylabel)
+        case "p1-p2-p3"
+            set(ax.XLabel, 'String', xlabel)
+            set(ax.YLabel, 'String', ylabel)
+            set(ax.ZLabel, 'String', zlabel)
+        otherwise
+            set(ax.YLabel, 'String', ylabel)
+            legend = set_legend(settings.target(i), chars);
+            set(ax.Legend, 'String', legend, 'Interpreter','latex');
+    end
+    if ftitle == 0
+        set(ax.Title, 'String', [])
+    end
+    set(ax.Legend, 'Location', 'northwest', 'FontSize', settings.fontsize-4);
+
+    if ~exist('plot/fig', 'dir')
+        mkdir('plot/fig')
+    end
     if fsave == 1
-        savefig(['plot/fig/', char(settings.target(i)), '.fig']);
+        savefig(['plot/fig/', erase(filename, '.mat'), char(settings.target(i)), '.fig']);
     elseif fsave == 2
-        savefig(['plot/fig/', char(settings.target(i)), '.png']);
+        savefig(['plot/fig/', erase(filename, '.mat'), char(settings.target(i)), '.png']);
     elseif fsave == 3
-        savefig(['plot/fig/', char(settings.target(i)), '.pdf']);
+        savefig(['plot/fig/', erase(filename, '.mat'), char(settings.target(i)), '.pdf']);
     end
 end
 
@@ -116,5 +170,49 @@ while true
     else
         fprintf('!!!%s is incorrect!!! ', att)
     end
+end
+end
+
+
+
+function legend = set_legend(target, chars)
+att_map = containers.Map({'e', 's', 'r', 'p'},...
+    {'est.', 'sen.', 'ref.', 'pla.'});
+legend_num = numel(chars) * 3;
+legend = cell(1, legend_num);
+switch target
+    case "p"
+        for i=1:numel(chars)
+            legend{3*i-2} = "$x$ " + att_map(chars(i));
+            legend{3*i-1} = "$y$ " + att_map(chars(i));
+            legend{3*i} = "$z$ " + att_map(chars(i));
+        end
+    case "v"
+        for i=1:numel(chars)
+            legend{3*i-2} = "$v_x$ " + att_map(chars(i));
+            legend{3*i-1} = "$v_y$ " + att_map(chars(i));
+            legend{3*i} = "$v_z$ " + att_map(chars(i));
+        end
+    case "q"
+        for i=1:numel(chars)
+            legend{3*i-2} = "$\theta_{roll}$ " + att_map(chars(i));
+            legend{3*i-1} = "$\theta_{pitch}$ " + att_map(chars(i));
+            legend{3*i} = "$\theta_{yaw}$ " + att_map(chars(i));
+        end
+    case "w"
+        for i=1:numel(chars)
+            legend{3*i-2} = "$\omega_{roll}$ " + att_map(chars(i));
+            legend{3*i-1} = "$\omega_{pitch}$ " + att_map(chars(i));
+            legend{3*i} = "$\omega_{yaw}$ " + att_map(chars(i));
+        end
+    case "input"
+        legend_num = numel(chars) * 4;
+        legend = cell(1, legend_num);
+        for i=1:numel(chars)
+            legend{4*i-3} = "$Thrust$ " + att_map(chars(i));
+            legend{4*i-2} = "$roll$ " + att_map(chars(i));
+            legend{4*i-1} = "$pitch$ " + att_map(chars(i));
+            legend{4*i} = "$yaw$ " + att_map(chars(i));
+        end
 end
 end
