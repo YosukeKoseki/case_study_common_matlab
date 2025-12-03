b6a78918bfd6976809911a15fb0f1704cf3abce2
Kudo2026 <g2212040@tcu.ac.jp>
2025-12-03 18:30:38 +0900

    周期15だときれい

--- sample/Controller_MPC_K.m
+++ sample/Controller_MPC_K.m
@@ -37,8 +37,8 @@ function Controller = Controller_MPC_K(dt, model_file, agent)
     %% sim用　重み かなり難しい
     Controller.weight.P = diag([80;80;100]);    % 位置　10,20刻み  20;1;30
     Controller.weight.Q = diag([150;150;250]);    % 姿勢角15良い気がする
-    Controller.weight.V = 20*diag([2;2;2]);% 速度  10,20刻み  30;20;10
-    Controller.weight.W = 100*diag([0.1;0.1;0.1]);  %角速度　1,2刻み 
+    Controller.weight.V = diag([40;40;40]);% 速度  10,20刻み  30;20;10
+    Controller.weight.W = diag([10;10;10]);  %角速度　1,2刻み 
     Controller.weight.R = diag([2; 10; 10; 15]); % 入力
     Controller.weight.RP =0*diag([40; 20; 20; 20]);  % 1ステップ前の入力との差    0*(無効化)
     %%　実験用　重み
